18d1c6d47129d3c00696f3ae4d796941c5506eab
Nick Hudson <njhudson25@gmail.com>
2023-03-16 20:10:17 +0000

    Task 1

---
+++ src/Animal.java
@@ -0,0 +1,12 @@
+public class Animal {
+    private Integer age;
+    public Animal() {}
+
+    public Integer getAge() {
+        return this.age;
+    }
+
+    public void setAge(Integer newAge){
+        this.age = newAge;
+    }
+}
